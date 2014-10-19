c32bd318bdbc7e481fe964c6b1ae8b0db934e748
Renato Cavalcanti <renato@strongtyped.io>
2014-10-20 01:48:12 +0200

    step 1: returning record Id

--- src/main/scala/slick/SlickApp.scala
+++ src/main/scala/slick/SlickApp.scala
@@ -11,18 +11,26 @@ object SlickApp extends App {
     createSchema
 
     val scalaIo     = Conferences += Conference("Scala.IO 2014", "Paris")
-    val scalax      = Conferences += Conference("ScalaExchange", "London")
-    val scalaDays   = Conferences += Conference("Scala Days 2015", "SF")
 
-    println(
-      s"""
-         |
-         |-------------------------------------
-         |Scala.IO        = $scalaIo
-         |Scala eXchange  = $scalax
-         |Scala Days      = $scalaDays
-         |-------------------------------------
-       """.stripMargin
-    )
+    val scalaxNoId = Conference("ScalaExchange", "London")
+    val scalaxId = (Conferences returning Conferences.map(_.id)) += scalaxNoId
+    val scalax = scalaxNoId.copy(id = Some(scalaxId))
+
+    val scalaDays = ((Conferences returning Conferences.map(_.id))
+      into ((conf, id) => conf.copy(id = Some(id) ) )
+      ) += Conference("Scala Days 2015", "SF")
+
+    printBlock {
+      s"""|Scala.IO        = $scalaIo
+          |Scala eXchange  = $scalax
+          |Scala Days      = $scalaDays """.stripMargin
+    }
+  }
+
+  def printBlock(block: => String): Unit = {
+    println()
+    println("-------------------------------------")
+    println(block)
+    println("-------------------------------------")
   }
 }
